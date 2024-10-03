169d4b00bd30d41f63e21af31edca4478997c0b7
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2024-10-03 20:04:19 +0200

    feat: check/wait deployment status added

--- src/kubeclient/KubeClient.ts
+++ src/kubeclient/KubeClient.ts
@@ -1,6 +1,8 @@
 import { KubernetesObject } from "../types";
 import { Kind } from "../types/Kind";
 
+const RETRY_MS = 5000;
+
 type KubeClientOptions = {
     url: string;
     token: string;
@@ -42,7 +44,50 @@ export class KubeClient {
             throw new Error(`Failed to create ${kubernetesObject}: ${response.statusText} - ${errorText}`);
         }
 
+        const json = await response.json();
         // TODO import logger
+        // console.log(JSON.stringify(json, null, 2));
         console.log(`${kubernetesObject} successfully applied.`);
+    }
+
+    async check({ kubernetesObject }: { kubernetesObject: KubernetesObject }): Promise<boolean> {
+        const url = this.getEndpoint(kubernetesObject.kind);
+        const response = await fetch(`${url}/${kubernetesObject.metadata.name}`, {
+            method: 'GET',
+            headers: {
+                "Authorization": `Bearer ${this.token}`,
+                "Content-Type": "application/json",
+            },
+        })
+
+        // TODO add logic to handle failures
+        if (!response.ok) {
+            const errorText = await response.text();
+            throw new Error(`Failed to create ${kubernetesObject}: ${response.statusText} - ${errorText}`);
+        }
+
+        const deployment = await response.json();
+        const desiredReplicas = deployment.spec?.replicas || 1;
+        const availableReplicas = deployment.status?.availableReplicas || 0;
+
+        return (availableReplicas === desiredReplicas)
+            ? true
+            : false
+    }
+
+    async wait({ kubernetesObject }: { kubernetesObject: KubernetesObject }): Promise<void> {
+        let isReady = false;
+        while(!isReady) {
+            // TODO import logger
+            console.log(`Waiting for ${kubernetesObject.metadata.name }`);
+            isReady = await this.check({ kubernetesObject });
+            if(!isReady) {
+                // TODO import logger
+                console.log(`Not ready... Retrying in ${RETRY_MS} milliseconds`)
+                await new Promise((resolve) => setTimeout(resolve, RETRY_MS))
+            }
+        }
+        // TODO import logger
+        console.log('ready')
     }
 }
